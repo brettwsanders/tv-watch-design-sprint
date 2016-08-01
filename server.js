becf1d335439ab3539289879641f4bf57e5bd662
Brett Sanders <brett.sanders@vevo.com>
2016-08-01 11:37:55 -0700

    update port for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.local.config');
 
 var app = new require('express')();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
