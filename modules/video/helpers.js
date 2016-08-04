befa23f3b752900a4e7a18594414f99532627947
Brett Sanders <brett.sanders@vevo.com>
2016-08-04 11:11:37 -0700

    remove console.log

--- modules/video/helpers.js
+++ modules/video/helpers.js
@@ -1,6 +1,5 @@
 export function getRandom(videos) {
   const length = videos.length;
   const randomIndex = Math.floor(Math.random() * (length - 1));
-  console.log('getRandom::length', length, 'randomIndex', randomIndex);
   return videos[randomIndex];
 }
