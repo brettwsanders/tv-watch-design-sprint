4e2f64ad7d7256acb9dd6ba57209e623fe7c516d
Brett Sanders <brett.sanders@vevo.com>
2016-08-05 17:57:17 -0700

    add progress bar

--- routes/home/components/home/home.js
+++ routes/home/components/home/home.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import styles from './home.scss';
 
 export class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      progress: '0%'
+    }
+  }
 
   componentDidMount() {
     window.setTimeout(() => {
       this.props.getVideo()
-    }, 1000)
+    }, 1000);
 
     //show cursor on mousemove for 5 seconds
     window.addEventListener('mousemove', () => {
@@ -17,12 +23,20 @@ export class Home extends Component {
     });
   }
 
-  shouldComponentUpdate(nextProps) {
-    const shouldUpdate = this.props.stream !== nextProps.stream;
-    return shouldUpdate;
+  onTimeUpdate() {
+    const width = `${(100 * (this.videoElement.currentTime / this.videoElement.duration)).toFixed(2)}%`;
+    this.setState({ progress: width });
   }
 
   render() {
+    const divStyle = {
+      position: 'fixed',
+      bottom: 0,
+      height: '0.2rem',
+      width: this.state.progress,
+      background: 'white',
+      transition: 'width 2s'
+    };
     return (
       <div className={ styles.default }>
         <div className={ styles.logo }>
@@ -36,6 +50,7 @@ export class Home extends Component {
           autoPlay
           onEnded={this.props.getVideo}
           src={this.props.stream}
+          onTimeUpdate={() => this.onTimeUpdate()}
         >
         </video>
         <div className={ styles.metadata }>
@@ -43,6 +58,7 @@ export class Home extends Component {
           <p><span>{this.props.video.title}</span></p>
           <p>{'vevo-tv-party.herokuapp.com'}</p>
         </div>
+        <div style={divStyle}></div>
       </div>
     );
   }
